f6c897b98eaf9538143e23cd7010f64bf6b0d9d3
Stas Ukolov <ukoloff@gmail.com>
2020-08-13 15:20:04 +0500

    Follow

--- src/tweeter.nim
+++ src/tweeter.nim
@@ -5,13 +5,19 @@ import view/[user, general], db
 
 let dbh = newDB()
 
+proc userLogin(db: DBase, request: Request, user: var User): bool =
+  if request.cookies.hasKey("username"):
+    if not db.findUser(request.cookies["username"], user):
+      user = User(name: request.cookies["username"], following: @[])
+      db.create(user)
+    return true
+  else:
+    return false
+
 routes:
   get "/":
-    if request.cookies.hasKey("username"):
-      var user: User
-      if not dbh.findUser(request.cookies["username"], user):
-        user = User(name: request.cookies["username"], following: @[])
-        dbh.create(user)
+    var user: User
+    if dbh.userLogin(request, user):
       let messages = dbh.findMessages(user.following & user.name)
       resp renderMain(renderTimeline(user.name, messages))
     else:
@@ -30,12 +36,28 @@ routes:
     dbh.post(message)
     redirect("/")
 
+  post "/follow":
+    var follower: User
+    var target: User
+    if not dbh.findUser(@"follower", follower):
+      halt "Follower not found"
+    if not dbh.findUser(@"target", target):
+      halt "Follow target not found"
+    dbh.follow(follower, target)
+    redirect(uri("/" & @"target"))
+
   get "/@name":
     cond '.' notin @"name"
     var user: User
     if not dbh.findUser(@"name", user):
       halt "User not found"
     let messages = dbh.findMessages(@[user.name])
-    resp renderMain(renderUser(user) & renderMessages(messages))
+
+    var me: User
+    if dbh.userLogin(request, me):
+      resp renderMain(renderUser(user, me) & renderMessages(messages))
+    else:
+      resp renderMain(renderUser(user) & renderMessages(messages))
+
 
 runForever()
