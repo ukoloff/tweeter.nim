9ac2f37731de9b8d1677793a094b28ef37c547e5
Stas Ukolov <ukoloff@gmail.com>
2020-08-13 16:12:32 +0500

    Fix deprecation warning

--- src/view/user.nim
+++ src/view/user.nim
@@ -32,7 +32,7 @@
 #for message in messages:
   <div>
     <a href="/@${message.user}">${message.user}</a>
-    <span>${message.time.getGMTime().format("HH:mm MMMM d',' yyyy")}</span>
+    <span>${message.time.utc().format("HH:mm MMMM d',' yyyy")}</span>
     <h3>${message.msg}</h3>
   </div>
 #end for
